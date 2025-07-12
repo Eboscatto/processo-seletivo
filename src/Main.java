e03ff5f3f7f59558a5fe0f939b4144d61a32b833
Eboscaatto <e_boscatto@hotmail.com>
2025-07-12 19:33:43 -0300

    Projeto inicializado

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import static candidatura.ProcessoSeletivo.analisarCandidatos;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        analisarCandidatos(1900.0);
+        analisarCandidatos(2200.0);
+        analisarCandidatos(2000.0);
+    }
+}
---
+++ src/candidatura/ProcessoSeletivo.java
@@ -0,0 +1,16 @@
+package candidatura;
+
+public class ProcessoSeletivo {
+    public static void analisarCandidatos(double salarioPretendido) {
+        double salarioBase = 2000.0;
+        if(salarioBase > salarioPretendido) {
+            System.out.println("LIGAR PARA O CANDIDATO");
+        }else if(salarioBase == salarioPretendido)
+            System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
+
+        else {
+            System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS");
+        }
+    }
+
+}
