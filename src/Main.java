8f46bc3084f50fa24d4451e64f403a99e8bc1162
Eboscaatto <e_boscatto@hotmail.com>
2025-07-15 22:11:50 -0300

    Implementados métodos

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,31 @@
+import java.util.List;
+
 import static candidatura.ProcessoSeletivo.analisarCandidatos;
+import static candidatura.ProcessoSeletivo.imprimirSelecionados;
+import static candidatura.ProcessoSeletivo.selecionarCandidatos;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("BEM VINDO AO SISTEMA DE PROCESSO  SELETIVO!");
+
+        //chamar o método analisarCandidatos
+
+        System.out.println("\n----------- Exibindo as mensagens -----------:");
 
         analisarCandidatos(1900.0);
         analisarCandidatos(2200.0);
         analisarCandidatos(2000.0);
+
+        System.out.println();
+
+        System.out.println("\n--------------------- Lista de inscritos ---------------------:");
+
+        // chama o método selecionarCandidatos
+        selecionarCandidatos();
+
+        List<String> selecionados = selecionarCandidatos(); // chama o método e recebe a lista
+        imprimirSelecionados(selecionados); // passa a lista para o método de impressão
     }
 }
--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,7 +1,12 @@
 package candidatura;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
     public static void analisarCandidatos(double salarioPretendido) {
+
         double salarioBase = 2000.0;
         if(salarioBase > salarioPretendido) {
             System.out.println("LIGAR PARA O CANDIDATO");
@@ -11,6 +16,48 @@ public class ProcessoSeletivo {
         else {
             System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS");
         }
+    }
+
+    public static List<String> selecionarCandidatos() {
+
+        String [] todosCandidatos = {"FELIPE","MARCIA","JULIA","PAULO","AUGUSTO","MONICA","FABRICIO","DANIELA","JORGE"};
+
+        List<String> candidatosSelecionadosLista = new ArrayList<>();
+
+                int candidatosSelecionados = 0;
+                int candidatosAtualizdos = 0;
+                double salarioBase = 2000.0;
+                while(candidatosSelecionadosLista.size() < 5 && candidatosAtualizdos < todosCandidatos.length) {
+                    String candidatoAtual = todosCandidatos[candidatosAtualizdos];
+                    double salarioPretendido = valorPretendido();
+
+                    System.out.println("O candidato " + candidatoAtual + " solicitou este valor de salário " + salarioPretendido);
+                    if(salarioBase >= salarioPretendido) {
+                        System.out.println("O candidato " + candidatoAtual + " foi selecionado pra a vaga.");
+                        candidatosSelecionadosLista.add(candidatoAtual);
+                    }
+                    candidatosAtualizdos++;
+
+                }
+                return candidatosSelecionadosLista;
+    }
+
+    private static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
+
+    public static void imprimirSelecionados(List<String> listaDeSelecionados) {
+        System.out.println("\n--- Lista de candidatos selecionados dinâmicamente ---");
+
+        if (listaDeSelecionados.isEmpty()) {
+            System.out.println("Nenhum candidato foi selecionado ainda.");
+        } else {
+            for(String candidato:  listaDeSelecionados) {
+                System.out.println("_ " + candidato);
+            }
+        }
+
+        System.out.println("-----------------------------------------------------\n");
     }
 
 }
